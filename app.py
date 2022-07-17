5e448208f20c8e53d9065dd0753f370819f916c5
ben-tiki <51ir42824557@campus.economicas.uba.ar>
2022-07-17 16:00:37 -0300

    detalles-generales

--- app.py
+++ app.py
@@ -28,12 +28,10 @@ def generar_reporte():
             tipo_cuenta = request.form["tipo_cuenta"].lower()
 
             # se filtran las transacciones que coincidan con los filtros
-            transacciones = get_matches(
-                tipo_operacion, estado_operacion, tipo_cuenta)
+            transacciones = get_matches(tipo_operacion, estado_operacion, tipo_cuenta)
 
             # se llama a la funcion que genera el reporte, en base a las transacciones filtradas
-            get_report(tipo_operacion, estado_operacion,
-                       tipo_cuenta, transacciones)
+            get_report(tipo_operacion, estado_operacion, tipo_cuenta, transacciones)
 
             # se carga el template html, con el contenido del reporte
             return render_template("reporte.html")
@@ -46,7 +44,10 @@ def generar_reporte():
 
 @app.route('/generar_usuario', methods=[ 'GET',"POST"])
 def generar_data():
+
     try:
+
+        # se obtienen los campos del formulario y se transforman en lowercase
         nombre = request.form['nombre'].lower()
         apellido = request.form['apellido'].lower()
         dni = request.form['dni'].lower()
@@ -56,13 +57,18 @@ def generar_data():
         direccion_ciudad = request.form['direccion_ciudad'].lower()
         direccion_provincia = request.form['direccion_provincia'].lower()
         direccion_pais = request.form['direccion_pais'].lower()
-        registro = Cliente(nombre, apellido, dni, tipo_cuenta, direccion_calle,
-                           direccion_numero, direccion_ciudad, direccion_provincia, direccion_pais)
+
+        # se crea un objeto Cliente, con los datos del usuario
+        registro = Cliente(nombre, apellido, dni, tipo_cuenta, direccion_calle, direccion_numero, direccion_ciudad, direccion_provincia, direccion_pais)
+
+        # se guarda el objeto en un arcihvo json y en otro csv
         registro.guardarDatos()
         registro.guardarDatosCsv()
         
         return render_template("usuarios.html")
+
     except:
+
         return render_template("usuarios.html")
 
 
--- usuarios.py
+++ usuarios.py
@@ -1,10 +1,9 @@
 import json
 import random
-import csv
-import pathlib
-
 
 class Cliente():
+
+    """ clase que permite crear clientes, con sus atributos y guardarlos en un archivo json y csv"""
 
     def __init__(self, nombre, apellido, dni, tipo_cuenta, direccion_calle, direccion_numero, direccion_ciudad, direccion_provincia, direccion_pais):
 
@@ -20,11 +19,12 @@ class Cliente():
 
     #guardar json
     def guardarDatos(self):
-        rand = random.randrange(100000, 999999)
-        if not pathlib.Path(f"static/json/{self.nombre}_{self.apellido}.json").exists():
-            with open(f"static/json/{self.nombre}_{self.apellido}.json", 'a') as usuario:
+
+            id_cliente = random.randrange(100000, 999999)
+
+            with open(f"static/output/clientes.json", 'a') as usuarios_json:
                 json.dump({
-                    'numero': rand,
+                    'numero': id_cliente,
                     "nombre": self.nombre,
                     "apellido": self.apellido,
                     "dni":  self.dni,
@@ -36,34 +36,29 @@ class Cliente():
                         "provincia": self.direccion_provincia,
                         "pais": self.direccion_pais,
                     },
-                }, usuario)
+                }, usuarios_json)
 
-    # Guardar csv
+    # guarda el csv de los datos
     def guardarDatosCsv(self):
-        rand = random.randrange(100000, 999999)
-        archivo = f"static/csv/{self.nombre}_{self.apellido}.csv"
 
-        fieldnames = ["Numero", "Nombre", "Apellido", "Dni",
-                      "Tipo", "Calle", "Numero", "Ciudad", "Provincia", "Pais"]
+        # en caso de que ya exista, se agrega un nuevo registro. Si no, se crea el archivo
+        with open('static/output/clientes.csv','a') as f:
 
-        if not pathlib.Path(archivo).exists():
-            with open(archivo, 'w', newline='') as create_file:
-                writer = csv.DictWriter(
-                    create_file, fieldnames=fieldnames, delimiter=';')
-                writer.writeheader()
+            # se crea aleatoriamente un numero de cuenta
+            id_cliente = random.randrange(100000, 999999)
 
-        with open(archivo, 'a', newline='') as escribir_csv:
-            writer = csv.DictWriter(
-                escribir_csv, fieldnames=fieldnames, delimiter=';')
-            writer.writerow({
-                'numero': rand,
-                "nombre": self.nombre,
-                "apellido": self.apellido,
-                "dni":  self.dni,
-                "tipo": self.tipo_cuenta,
-                "calle": self.direccion_calle,
-                "numero": self.direccion_numero,
-                "ciudad": self.direccion_ciudad,
-                "provincia": self.direccion_provincia,
-                "pais": self.direccion_pais,
-            })
+            nombre = self.nombre
+            apellido = self.apellido
+            dni = self.dni
+            tipo_cuenta = self.tipo_cuenta
+            direccion_calle = self.direccion_calle
+            direccion_numero = self.direccion_numero
+            direccion_ciudad = self.direccion_ciudad
+            direccion_provincia = self.direccion_provincia
+            direccion_pais = self.direccion_pais
+
+            # se escribe el archivo, usando el ";" como separador
+            items = f"{id_cliente};{nombre};{apellido};{dni};{tipo_cuenta};{direccion_calle};{direccion_numero};{direccion_ciudad};{direccion_provincia};{direccion_pais}\n"
+
+            # se escribe el archivo
+            f.write(items)
